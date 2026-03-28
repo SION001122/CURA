4fe82eae4d57bdf1ff6c3883e7e3ddcb650d56ef
SION001122 <163120715+SION001122@users.noreply.github.com>
2026-03-28 17:32:51 +0900

    CURA.py

--- FALLALLD FALL/CURA.py
+++ FALLALLD FALL/CURA.py
@@ -4,22 +4,57 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import torch
 from torch.utils.data import Dataset, DataLoader
-
+from tqdm import tqdm
+from sklearn.model_selection import GroupShuffleSplit
 
 def load_and_window_csv_with_label(path, window_size=1800, stride=200):
-    df = pd.read_csv(path)
-    signal = df[['acc_x', 'acc_y', 'acc_z']].to_numpy()
-    raw_labels = df['label'].to_numpy()
 
-    windows = []
-    labels = []
-    for i in range(0, len(signal) - window_size + 1, stride):
-        window = signal[i:i + window_size]
-        label_window = raw_labels[i:i + window_size]
-        label = 1 if np.mean(label_window) > 0.5 else 0
-        windows.append(window)
-        labels.append(label)
-    return np.array(windows), np.array(labels)
+    df = pd.read_csv(path)
+
+
+    df['subject_id'] = df['source_file'].apply(lambda x: int(str(x).split('_')[0][1:]))
+
+    df['activity_id'] = df['source_file'].apply(lambda x: int(str(x).split('_')[2][1:]))
+
+    df['label'] = (df['activity_id'] >= 50).astype(int)
+
+    gss = GroupShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    train_idx, test_idx = next(gss.split(df, groups=df['subject_id']))
+    
+    train_df = df.iloc[train_idx].reset_index(drop=True)
+    test_df = df.iloc[test_idx].reset_index(drop=True)
+
+
+    train_signal_raw = train_df[['acc_x', 'acc_y', 'acc_z']].to_numpy()
+    train_mean = np.mean(train_signal_raw, axis=0)
+    train_std = np.std(train_signal_raw, axis=0) + 1e-7
+
+    def extract_windows(data_df, mean, std, desc):
+        signal = data_df[['acc_x', 'acc_y', 'acc_z']].to_numpy()
+        signal = (signal - mean) / std
+        
+        target_labels = data_df['label'].to_numpy()
+        
+        windows = []
+        labels = []
+        
+
+        for i in tqdm(range(0, len(signal) - window_size + 1, stride), desc=desc):
+            window = signal[i:i + window_size]
+
+            label_mean = np.mean(target_labels[i:i + window_size])
+            label = 1 if label_mean > 0.5 else 0
+            
+            windows.append(window)
+            labels.append(label)
+            
+        return np.array(windows), np.array(labels)
+
+    X_train, y_train = extract_windows(train_df, train_mean, train_std, "Extracting Train Windows")
+    X_test, y_test = extract_windows(test_df, train_mean, train_std, "Extracting Test Windows")
+
+
+    return X_train, y_train, X_test, y_test
 
 class FallDataset(Dataset):
     def __init__(self, X, y):
@@ -29,7 +64,14 @@ class FallDataset(Dataset):
         return len(self.X)
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
+import random
+from sklearn.metrics import accuracy_score, f1_score
 
+def focal_loss(inputs, targets, alpha=0.25, gamma=2.0):
+            BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction='none')
+            pt = torch.exp(-BCE_loss)
+            F_loss = alpha * (1-pt)**gamma * BCE_loss
+            return F_loss.mean()
 
 
 import torch.nn as nn
@@ -56,88 +98,71 @@ class CURA_CORE(nn.Module):
         return self.output(x_cnn).squeeze()
 
 import random
-SEED = 100
-random.seed(SEED)
-np.random.seed(SEED)
-torch.manual_seed(SEED)
-torch.cuda.manual_seed_all(SEED)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
 import torch.optim as optim
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 import matplotlib.pyplot as plt
-
 if __name__ == "__main__":
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        SEED = 34069  
 
-    X, y = load_and_window_csv_with_label("fall_waist_with_gyro.csv")
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+        random.seed(SEED)
+        np.random.seed(SEED)
+        torch.manual_seed(SEED)
+        torch.cuda.manual_seed_all(SEED)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        csv_path = r"fall_waist_with_gyro.csv"
+        X_train, y_train, X_test, y_test = load_and_window_csv_with_label(csv_path, window_size=1800, stride=200)
 
-    train_loader = DataLoader(FallDataset(X_train, y_train), batch_size=64, shuffle=True)
-    test_loader = DataLoader(FallDataset(X_test, y_test), batch_size=64)
+        train_loader = DataLoader(FallDataset(X_train, y_train), batch_size=64, shuffle=True)
+        test_loader = DataLoader(FallDataset(X_test, y_test), batch_size=64)
+        model = CURA_CORE().to(device)
+        optimizer = optim.Adam(model.parameters(), lr=0.0005)
 
-    model = CURA_CORE().to(device)
-    pos_weight = torch.tensor([(y_train == 0).sum() / (y_train == 1).sum()], device=device)
-    criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+        print("Model parameter count:", sum(p.numel() for p in model.parameters() if p.requires_grad))
 
-    print("Model parameter count:", sum(p.numel() for p in model.parameters() if p.requires_grad))
-    print("Train label count:", torch.bincount(torch.tensor(y_train).int()))
-    print("Test label count:", torch.bincount(torch.tensor(y_test).int()))
+        stopping_counter = 0
+        maxcount = 10
+        best_f1 = 0
+        loss_list, acc_list, f1_list, sensitivity_list, specificity_list, epoch_list = [], [], [], [], [], []
+
+        for epoch in tqdm(range(1, 100), desc="Training"):
+            model.train()
+            total_loss = 0
+            for X_batch, y_batch in train_loader:
+                X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+                optimizer.zero_grad()
+                output = model(X_batch)
+                loss = focal_loss(output.unsqueeze(1), y_batch.unsqueeze(1).float(), alpha=0.25, gamma=2.0)
+                loss.backward()
+                optimizer.step()
+                total_loss += loss.item()
 
 
-    loss_list, acc_list, f1_list, sensitivity_list, specificity_list, epoch_list = [], [], [], [], [], []
-
-    for epoch in range(1, 100):
-        model.train()
-        total_loss = 0
-        for X_batch, y_batch in train_loader:
-            X_batch, y_batch = X_batch.to(device), y_batch.to(device)
-            optimizer.zero_grad()
-            output = model(X_batch)
-            loss = criterion(output, y_batch)
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
-
-        if epoch % 10 == 0:
             model.eval()
             all_preds, all_labels = [], []
             with torch.no_grad():
-                for X_batch, y_batch in test_loader:
-                    X_batch = X_batch.to(device)
-                    logits = model(X_batch)
-                    probs = torch.sigmoid(logits)
-                    preds = (probs > 0.6).int().cpu().numpy()
-                    all_preds.extend(preds)
-                    all_labels.extend(y_batch.numpy())
+                    for X_batch, y_batch in test_loader:
+                        X_batch = X_batch.to(device)
+                        logits = model(X_batch)
+                        probs = torch.sigmoid(logits)
+                        preds = (probs > 0.4).int().cpu().numpy()
+                        all_preds.extend(preds)
+                        all_labels.extend(y_batch.numpy())
 
             acc = accuracy_score(all_labels, all_preds)
             f1 = f1_score(all_labels, all_preds)
-            tn, fp, fn, tp = confusion_matrix(all_labels, all_preds).ravel()
-            sensitivity = tp / (tp + fn) if (tp + fn) > 0 else 0
-            specificity = tn / (tn + fp) if (tn + fp) > 0 else 0
 
-            print(f"[Epoch {epoch}] Loss: {total_loss:.4f}, Acc: {acc:.4f}, F1: {f1:.4f}")
 
             epoch_list.append(epoch)
             loss_list.append(total_loss)
             acc_list.append(acc)
             f1_list.append(f1)
-            sensitivity_list.append(sensitivity)
-            specificity_list.append(specificity)
-            if f1_list[-1] > 0.8:
-                print("Early stopping at epoch", epoch)
-                break
-
-    plt.figure(figsize=(12, 5))
-    plt.subplot(1, 2, 1)
-    plt.plot(epoch_list, acc_list, marker='o', label='Accuracy')
-    plt.plot(epoch_list, f1_list, marker='s', label='F1-score')
-    plt.title("Accuracy & F1-score")
-    plt.xlabel("Epoch")
-    plt.legend()
-    plt.grid(True)
-
-    plt.tight_layout()
-    plt.show()
+            if f1 > best_f1:
+                best_f1 = f1
+                stopping_counter = 0
+            else:
+                stopping_counter += 1
+                if stopping_counter >= maxcount:
+                    print(f"Early stopping at epoch {epoch} with best F1: {best_f1:.4f}")
+                    break
